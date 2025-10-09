f1dc6b5b0756acda3fa1a2db2d3ebd5e356b9f49
DnsSera <165093395+DnsSera@users.noreply.github.com>
2025-10-09 13:56:29 -0400

    First commit
    Initial commit

---
+++ src/test/java/com/tesis/teamsoft/TeamSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tesis.teamsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamSoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
